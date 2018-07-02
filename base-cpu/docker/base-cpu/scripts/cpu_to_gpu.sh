6f21a060b2bcdb3ee4443ea49a6a89fba248fa93
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-07-01 23:57:12 -0400

    refactored numerous packages.

---
+++ base-cpu/docker/base-cpu/scripts/cpu_to_gpu.sh
@@ -0,0 +1,2 @@
+cp -R ../../../../carme-base-cpu/docker/carme-base-cpu ../../../../carme-base-gpu/docker/carme-base-gpu
+rm -rf ../../jupyter-base-gpu/scripts
---
+++ base-gpu/docker/base-gpu/README.md
@@ -0,0 +1,3 @@
+## Carme Base notebooks
+
+This base notebook is not meant to be used as a package directly.
